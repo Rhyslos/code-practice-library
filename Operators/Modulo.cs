14d7e5965a2595fd7d4eff8610f452d3196d7af9
Yuna Emiya <yuna-emiya@outlook.com>
2026-01-28 21:24:56 +0100

    Convert a number method and round down or up based on decimal value

--- Operators/Modulo.cs
+++ Operators/Modulo.cs
@@ -19,7 +19,29 @@ public class OddOrEvenCheck
 
         System.Console.WriteLine(isEven);
     }
+
+    public static double ConvertedNumber(double inputNumber)
+    {
+        double wholeNumber   = Math.Truncate(inputNumber);
+        double decimalNumber = inputNumber - wholeNumber;
+
+        int result;
+
+        if(decimalNumber >= .5)
+        {
+           result = (int)Math.Ceiling(inputNumber);
+           return result;
+        }
+        else
+        {
+            result = (int)Math.Floor(inputNumber);
+            return result;
+        }
+    }
 }
+
+
+
 
 
 // first modulo attempt, keeping because it's fun to see where i started from to what end result became
