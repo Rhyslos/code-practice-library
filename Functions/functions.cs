3c0b7e624a1d0935bcb754aafc2f2e5888087e3a
Yuna Emiya <yuna-emiya@outlook.com>
2026-01-28 21:34:49 +0100

    Added new methods for doing WriteLine and ReadLine purely through lazyness

---
+++ Functions/functions.cs
@@ -0,0 +1,17 @@
+
+namespace PracticeModulo.Functions;
+
+public static class Shortcuts
+{
+    // Lazy WriteLine
+    public static void write(object text)
+    {
+        Console.WriteLine(text);
+    }
+
+    // Lazy ReadLine
+    public static string read()
+    {
+        return Console.ReadLine();
+    }
+}
